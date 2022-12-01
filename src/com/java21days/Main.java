f09ad10fd7e1da430018ab9f1478369fbfc79513
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 21:03:32 -0500

    Complete main class

--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -1,4 +1,9 @@
 package com.java21days;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello. What's your name?");
+        String response = ResponseBuilder.readInput();
+        System.out.println("Hello, " + response);
+    }
 }
